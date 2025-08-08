9652e9911c51039cab1f9d642d3331e53f175ee0
MarcosPPazzinatto <marcospaulopazzinatto@gmail.com>
2025-08-08 08:02:10 -0300

    Creating basic template for akamai

---
+++ modules/property/main.tf
@@ -0,0 +1,8 @@
+# This file is intentionally left minimal.
+# You can start defining Akamai resources here, such as properties, groups, etc.
+
+# Example placeholder:
+# resource "akamai_property" "example" {
+#   ...
+# }
+
---
+++ outputs.tf
@@ -0,0 +1,8 @@
+# Output values will be defined here once resources are provisioned
+
+# Example placeholder:
+# output "property_id" {
+#   description = "The ID of the created Akamai property"
+#   value       = akamai_property.example.id
+# }
+
